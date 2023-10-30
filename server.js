aca648a33aac1a755cdc153611ad86b3856c0f8b
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-10-30 17:28:36 -0500

    New Server Setup -Added server.js, index.html, etc

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require("http");
+fs = require("fs");
+url = require("url");
+
+http
+	.createServer((request, response) => {
+		let addr = request.url,
+			q = new URL(addr, "http://localhost:8080"),
+			filePath = "";
+
+		fs.appendFile("log.txt", "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n", (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Added to log.");
+			}
+		});
+
+		if (q.pathname.includes("documentation")) {
+			filePath = __dirname + "/documentation.html";
+		} else {
+			filePath = __dirname + "/index.html";
+		}
+
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				console.error(err);
+				response.writeHead(500);
+				response.end("Internal Server Error");
+				return;
+			}
+
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.write(data);
+			response.end();
+		});
+	})
+	.listen(8080);
+
+console.log("My first Node test server is running on Port 8080.");
