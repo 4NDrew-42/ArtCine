71f5ee8aed977e4654839cc34955122cb188b526
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-12-05 20:06:41 -0600

    2.10
    CONNECTION_URI,
    mongodb updated

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://localhost:27017/ACDB", { useNewUrlParser: true, useUnifiedTopology: true });
+/*mongoose.connect("mongodb://localhost:27017/ACDB", { useNewUrlParser: true, useUnifiedTopology: true });*/
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
