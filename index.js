aad03577477b00413ce2a1f0163fdfe4e76538a7
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2024-03-08 14:50:20 -0600

    fix - allow methods and headers

--- index.js
+++ index.js
@@ -38,12 +38,13 @@ app.use(
 		origin: (origin, callback) => {
 			if (!origin) return callback(null, true);
 			if (allowedOrigins.indexOf(origin) === -1) {
-				// If a specific origin isn't found on the list of allowed origins
 				let message = "The CORS policy for this application doesn't allow access from origin " + origin;
 				return callback(new Error(message), false);
 			}
 			return callback(null, true);
 		},
+		methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+		allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
 	})
 );
 
